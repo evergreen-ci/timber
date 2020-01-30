10da15d03016baca2cf89470004359ee42cd2d51
julianedwards <julian.edwards@10gen.com>
2020-01-30 16:58:29 -0500

    MAKE-1133: Revendor grip (with updated Sender interface) (#13)

--- vendor/github.com/mongodb/grip/send/systemd.go
+++ vendor/github.com/mongodb/grip/send/systemd.go
@@ -3,6 +3,7 @@
 package send
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"log"
@@ -56,17 +57,19 @@ func MakeSystemdLogger() (Sender, error) {
 func (s *systemdJournal) Send(m message.Composer) {
 	defer func() {
 		if err := recover(); err != nil {
-			s.ErrorHandler(fmt.Errorf("panic: %v", err), m)
+			s.ErrorHandler()(fmt.Errorf("panic: %v", err), m)
 		}
 	}()
 
 	if s.Level().ShouldLog(m) {
 		err := journal.Send(m.String(), s.level.convertPrioritySystemd(m.Priority()), s.options)
 		if err != nil {
-			s.ErrorHandler(err, m)
+			s.ErrorHandler()(err, m)
 		}
 	}
 }
+
+func (s *systemdJournal) Flush(_ context.Context) error { return nil }
 
 func (l LevelInfo) convertPrioritySystemd(p level.Priority) journal.Priority {
 	switch p {
