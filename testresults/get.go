407737e0321077a4df00f6f6c7e8072ea9423056
julianedwards <julian.edwards@mongodb.com>
2021-09-22 18:57:45 -0400

    add new routes and tests

--- testresults/get.go
+++ testresults/get.go
@@ -6,47 +6,59 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/evergreen-ci/timber"
 	"github.com/mongodb/grip"
 	"github.com/pkg/errors"
 )
 
-// TestResultsGetOptions specify the required and optional information to
-// create the test results HTTP GET request to cedar.
-type TestResultsGetOptions struct {
+// Valid sort by keys.
+const (
+	SortByStart      = "start"
+	SortByDuration   = "duration"
+	SortByTestName   = "test_name"
+	SortByStatus     = "status"
+	SortByBaseStatus = "base_status"
+)
+
+// GetOptions specify the required and optional information to create the test
+// results HTTP GET request to cedar.
+type GetOptions struct {
 	CedarOpts timber.GetOptions
 
 	// Request information. See cedar's REST documentation for more
 	// information:
 	// `https://github.com/evergreen-ci/cedar/wiki/Rest-V1-Usage`.
 	TaskID       string
-	TestName     string
-	Execution    *int
 	FailedSample bool
 	Stats        bool
+
+	// Query parameters.
+	Execution    *int
 	DisplayTask  bool
+	TestName     string
+	Statuses     []string
+	GroupID      string
+	SortBy       string
+	SortOrderDSC bool
+	BaseTaskID   string
+	Limit        int
+	Page         int
 }
 
 // Validate ensures TestResultsGetOptions is configured correctly.
-func (opts *TestResultsGetOptions) Validate() error {
+func (opts GetOptions) Validate() error {
 	catcher := grip.NewBasicCatcher()
 
 	catcher.Add(opts.CedarOpts.Validate())
 	catcher.NewWhen(opts.TaskID == "", "must provide a task id")
-	catcher.NewWhen(opts.FailedSample && opts.TestName != "", "cannot request the failed sample when requesting a single test result")
 	catcher.NewWhen(opts.FailedSample && opts.Stats, "cannot request the failed sample and stats, must be one or the other")
 
 	return catcher.Resolve()
 }
 
-// GetTestResults returns with the test results requested via HTTP to a cedar
-// service.
-func GetTestResults(ctx context.Context, opts TestResultsGetOptions) ([]byte, error) {
-	if err := opts.Validate(); err != nil {
-		return nil, errors.WithStack(err)
-	}
-
+func (opts GetOptions) getURL() string {
 	urlString := fmt.Sprintf("%s/rest/v1/test_results/task_id/%s", opts.CedarOpts.BaseURL, url.PathEscape(opts.TaskID))
 	if opts.FailedSample {
 		urlString += "/failed_sample"
@@ -55,22 +67,54 @@ func GetTestResults(ctx context.Context, opts TestResultsGetOptions) ([]byte, er
 		urlString += "/stats"
 	}
 
-	var params string
-	if opts.TestName != "" {
-		params += fmt.Sprintf("test_name=%s", opts.TestName)
-	}
+	var params []string
 	if opts.Execution != nil {
-		params += fmt.Sprintf("execution=%d", opts.Execution)
+		params = append(params, fmt.Sprintf("execution=%d", *opts.Execution))
 	}
 	if opts.DisplayTask {
-		params += "display_task=true"
+		params = append(params, "display_task=true")
+	}
+	if opts.TestName != "" {
+		params = append(params, fmt.Sprintf("test_name=%s", opts.TestName))
+	}
+	if len(opts.Statuses) > 0 {
+		for _, status := range opts.Statuses {
+			params = append(params, fmt.Sprintf("status=%s", status))
+		}
+	}
+	if opts.GroupID != "" {
+		params = append(params, fmt.Sprintf("group_id=%s", opts.GroupID))
+	}
+	if opts.SortBy != "" {
+		params = append(params, fmt.Sprintf("sort_by=%s", opts.SortBy))
+	}
+	if opts.SortOrderDSC {
+		params = append(params, "sort_order_dsc=true")
+	}
+	if opts.BaseTaskID != "" {
+		params = append(params, fmt.Sprintf("base_task_id=%s", opts.BaseTaskID))
+	}
+	if opts.Limit > 0 {
+		params = append(params, fmt.Sprintf("limit=%d", opts.Limit))
+	}
+	if opts.Page > 0 {
+		params = append(params, fmt.Sprintf("page=%d", opts.Page))
 	}
 	if len(params) > 0 {
-		urlString += "?" + params
+		urlString += "?" + strings.Join(params, "&")
+	}
+
+	return urlString
+}
+
+// Get returns with the test results requested via HTTP to a cedar service.
+func Get(ctx context.Context, opts GetOptions) ([]byte, error) {
+	if err := opts.Validate(); err != nil {
+		return nil, errors.WithStack(err)
 	}
 
 	catcher := grip.NewBasicCatcher()
-	resp, err := opts.CedarOpts.DoReq(ctx, urlString)
+	resp, err := opts.CedarOpts.DoReq(ctx, opts.getURL())
 	if err != nil {
 		return nil, errors.Wrap(err, "requesting test results from cedar")
 	}
--- testresults/get_test.go
+++ testresults/get_test.go
@@ -7,22 +7,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTestResultsGetOptionsValidate(t *testing.T) {
+func TestGetOptionsValidate(t *testing.T) {
 	for _, test := range []struct {
 		name   string
-		opts   TestResultsGetOptions
+		opts   GetOptions
 		hasErr bool
 	}{
 		{
-			name: "FailsWithInvalidCedarOpts",
-			opts: TestResultsGetOptions{
+			name: "InvalidCedarOpts",
+			opts: GetOptions{
 				TaskID: "task",
 			},
 			hasErr: true,
 		},
 		{
-			name: "FailsWithMissingTaskID",
-			opts: TestResultsGetOptions{
+			name: "MissingTaskID",
+			opts: GetOptions{
 				CedarOpts: timber.GetOptions{
 					BaseURL: "https://url.com",
 				},
@@ -30,20 +30,20 @@ func TestTestResultsGetOptionsValidate(t *testing.T) {
 			hasErr: true,
 		},
 		{
-			name: "FailsWithTestNameAndFailedSample",
-			opts: TestResultsGetOptions{
+			name: "FailedSampleAndStats",
+			opts: GetOptions{
 				CedarOpts: timber.GetOptions{
 					BaseURL: "https://url.com",
 				},
-				TaskID:       "display",
-				TestName:     "test",
+				TaskID:       "task",
 				FailedSample: true,
+				Stats:        true,
 			},
 			hasErr: true,
 		},
 		{
-			name: "SucceedsWithTaskID",
-			opts: TestResultsGetOptions{
+			name: "TaskID",
+			opts: GetOptions{
 				CedarOpts: timber.GetOptions{
 					BaseURL: "https://url.com",
 				},
@@ -51,8 +51,8 @@ func TestTestResultsGetOptionsValidate(t *testing.T) {
 			},
 		},
 		{
-			name: "SucceedsWithTaskIDAndFailedSample",
-			opts: TestResultsGetOptions{
+			name: "TaskIDAndFailedSample",
+			opts: GetOptions{
 				CedarOpts: timber.GetOptions{
 					BaseURL: "https://url.com",
 				},
@@ -61,45 +61,13 @@ func TestTestResultsGetOptionsValidate(t *testing.T) {
 			},
 		},
 		{
-			name: "SucceedsWithDisplayTaskID",
-			opts: TestResultsGetOptions{
+			name: "TaskIDAndStats",
+			opts: GetOptions{
 				CedarOpts: timber.GetOptions{
 					BaseURL: "https://url.com",
 				},
-				TaskID:      "display",
-				DisplayTask: true,
-			},
-		},
-		{
-			name: "SucceedsWithDisplayTaskIDAndFailedSample",
-			opts: TestResultsGetOptions{
-				CedarOpts: timber.GetOptions{
-					BaseURL: "https://url.com",
-				},
-				TaskID:       "display",
+				TaskID:       "task",
 				FailedSample: true,
-				DisplayTask:  true,
-			},
-		},
-		{
-			name: "SucceedsWithTaskIDAndTestName",
-			opts: TestResultsGetOptions{
-				CedarOpts: timber.GetOptions{
-					BaseURL: "https://url.com",
-				},
-				TaskID:   "task",
-				TestName: "test",
-			},
-		},
-		{
-			name: "SucceedsWithDisplayTaskIDAndTestName",
-			opts: TestResultsGetOptions{
-				CedarOpts: timber.GetOptions{
-					BaseURL: "https://url.com",
-				},
-				TaskID:      "task",
-				TestName:    "test",
-				DisplayTask: true,
 			},
 		},
 	} {
@@ -110,6 +78,66 @@ func TestTestResultsGetOptionsValidate(t *testing.T) {
 			} else {
 				assert.NoError(t, err)
 			}
+		})
+	}
+}
+
+func TestGetURL(t *testing.T) {
+	cedarOpts := timber.GetOptions{BaseURL: "https://url.com"}
+	baseURL := cedarOpts.BaseURL + "/rest/v1/test_results"
+	exec := 1
+	for _, test := range []struct {
+		name        string
+		opts        GetOptions
+		expectedURL string
+	}{
+		{
+			name: "TaskID",
+			opts: GetOptions{
+				CedarOpts: cedarOpts,
+				TaskID:    "task",
+			},
+			expectedURL: baseURL + "/task_id/task",
+		},
+		{
+			name: "TaskIDWithParams",
+			opts: GetOptions{
+				CedarOpts:    cedarOpts,
+				TaskID:       "task",
+				Execution:    &exec,
+				DisplayTask:  true,
+				TestName:     "test",
+				Statuses:     []string{"fail", "silentfail"},
+				GroupID:      "group",
+				SortBy:       "sort",
+				SortOrderDSC: true,
+				BaseTaskID:   "base_task",
+				Limit:        100,
+				Page:         5,
+			},
+			expectedURL: baseURL + "/task_id/task?execution=1&display_task=true&test_name=test&status=fail&status=silentfail&group_id=group&sort_by=sort&sort_order_dsc=true&base_task_id=base_task&limit=100&page=5",
+		},
+		{
+			name: "FailedSample",
+			opts: GetOptions{
+				CedarOpts:    cedarOpts,
+				TaskID:       "task",
+				FailedSample: true,
+			},
+			expectedURL: baseURL + "/task_id/task/failed_sample",
+		},
+		{
+			name: "Stats",
+			opts: GetOptions{
+				CedarOpts: cedarOpts,
+				TaskID:    "task",
+				Stats:     true,
+			},
+			expectedURL: baseURL + "/task_id/task/stats",
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.expectedURL, test.opts.getURL())
 		})
 	}
 }
