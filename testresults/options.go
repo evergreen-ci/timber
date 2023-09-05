88c53a14c47b5f8604d64590538327594b6ff052
julianedwards <julian.edwards@10gen.com>
2023-09-05 14:40:25 -0400

    EVG-20020: update test results client with new log fields (#150)

--- testresults/options.go
+++ testresults/options.go
@@ -68,13 +68,16 @@ type Result struct {
 	GroupID         string    `bson:"group_id" json:"group_id" yaml:"group_id"`
 	Trial           int32     `bson:"trial" json:"trial" yaml:"trial"`
 	Status          string    `bson:"status" json:"status" yaml:"status"`
-	LogTestName     string    `bson:"log_test_name" json:"log_test_name" yaml:"log_test_name"`
-	LogURL          string    `bson:"log_url" json:"log_url" yaml:"log_url"`
-	RawLogURL       string    `bson:"raw_log_url" json:"raw_log_url" yaml:"raw_log_url"`
-	LineNum         int32     `bson:"line_num" json:"line_num" yaml:"line_num"`
+	LogInfo         *LogInfo  `bson:"log_info" json:"log_info" yaml:"log_info"`
 	TaskCreated     time.Time `bson:"task_created" json:"task_created" yaml:"task_created"`
 	TestStarted     time.Time `bson:"test_started" json:"test_started" yaml:"test_started"`
 	TestEnded       time.Time `bson:"test_ended" json:"test_ended" yaml:"test_ended"`
+
+	// Legacy test log fields.
+	LogTestName string `bson:"log_test_name" json:"log_test_name" yaml:"log_test_name"`
+	LogURL      string `bson:"log_url" json:"log_url" yaml:"log_url"`
+	RawLogURL   string `bson:"raw_log_url" json:"raw_log_url" yaml:"raw_log_url"`
+	LineNum     int32  `bson:"line_num" json:"line_num" yaml:"line_num"`
 }
 
 // export converts a Result into the equivalent protobuf TestResult.
@@ -85,6 +88,7 @@ func (r Result) export() *gopb.TestResult {
 		GroupId:         r.GroupID,
 		Trial:           r.Trial,
 		Status:          r.Status,
+		LogInfo:         r.LogInfo.export(),
 		LogTestName:     r.LogTestName,
 		LogUrl:          r.LogURL,
 		RawLogUrl:       r.RawLogURL,
@@ -92,5 +96,30 @@ func (r Result) export() *gopb.TestResult {
 		TaskCreateTime:  timestamppb.New(r.TaskCreated),
 		TestStartTime:   timestamppb.New(r.TestStarted),
 		TestEndTime:     timestamppb.New(r.TestEnded),
+	}
+}
+
+// LogInfo describes a metadata for a result's log stored using Evergreen
+// logging.
+type LogInfo struct {
+	LogName       string
+	LogsToMerge   []string
+	LineNum       int32
+	RenderingType *string
+	Version       int32
+}
+
+// export converts LogInfo into the equivalent protobuf TestLogInfo.
+func (li *LogInfo) export() *gopb.TestLogInfo {
+	if li == nil {
+		return nil
+	}
+
+	return &gopb.TestLogInfo{
+		LogName:       li.LogName,
+		LogsToMerge:   li.LogsToMerge,
+		LineNum:       li.LineNum,
+		RenderingType: li.RenderingType,
+		Version:       li.Version,
 	}
 }
